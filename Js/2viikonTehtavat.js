77d3faad9c5d543337ca3b7494b9edf1abffed70
Mika Pouramo <mika.pouramo@edu.keuda.fi>
2021-03-11 13:42:39 +0200

    edit 3

--- Js/2viikonTehtavat.js
+++ Js/2viikonTehtavat.js
@@ -38,3 +38,76 @@ function jarjesta()
       }
     }
   }
+  function etsiSuurin()
+  {
+    var l1, l2, l3, l4, l5, suurin;
+    l1 = parseInt(document.getElementById('luku1').value);
+    l2 = parseInt(document.getElementById('luku2').value);
+    l3 = parseInt(document.getElementById('luku3').value);
+    l4 = parseInt(document.getElementById('luku4').value);
+    l5 = parseInt(document.getElementById('luku5').value);
+    if(l1 < l2 && l3 < l2 && l4 < l2 && l5 < l2)
+    {
+      suurin = l2;
+    }
+    else if(l1 < l3 && l4 < l3 && l5 < l3)
+    {
+      suurin = l3;
+    }
+    else if(l1 < l4 && l5 < l4)
+    {
+      suurin = l4;
+    }
+    else if(l1 < l5)
+    {
+      suurin = l5;
+    }
+    else {
+      suurin = l1;
+    }
+    document.write("Annoit luvut: " + l1 + ' ' + l2 + ' ' + l3 + ' ' + l4 + ' ' + l5);
+    document.write("<br>Suurin niistÃ¤ on: " + suurin);
+  }
+  function paritonParillinen()
+  {
+    var arvo;
+    arvo = parseInt(document.getElementById('luku').value);
+    document.write("Antamasi luku: " + arvo);
+    if(arvo%2 == 0)
+    {
+      document.write("<br>Antamasi luku on parillinen");
+    }
+    else {
+      document.write("<br>Antamasi luku on pariton");
+    }
+  }
+  function ajoneuvo()
+  {
+    var age = parseInt(document.getElementById('ika').value);
+    if(age < 16)
+    {
+      document.write("Voit ajaa polkupyÃ¶rÃ¤Ã¤");
+    }
+    else if(age < 18)
+    {
+      document.write("Voit ajaa mopoa");
+    }
+    else {
+      document.write("Voit ajaa autoa");
+    }
+  }
+  function kaanna()
+  {
+    var lang = document.getElementById('kieli').value;
+    if(lang == 'en')
+    {
+      document.write("Hello world!");
+    }
+    else if(lang == 'sw')
+    {
+      document.write("Hej världen!");
+    }
+    else {
+      document.write("Hola Mundo!");
+    }
+  }
