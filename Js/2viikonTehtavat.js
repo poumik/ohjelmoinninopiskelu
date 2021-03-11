ab30c7f8ab4b5d4dd3920210b7f11057018ea210
Mika Pouramo <mika.pouramo@edu.keuda.fi>
2021-03-11 13:33:08 +0200

    edit 3

--- Js/2viikonTehtavat.js
+++ Js/2viikonTehtavat.js
@@ -1,28 +1,28 @@
 function jarjesta()
 {
   var luku1, luku2, luku3;
-  luku1 = parseInt(document.getElementById('eka').value);
-  luku2 = parseInt(document.getElementById('toka').value);
+  luku1 = parseInt(document.getElementById('ensimmainen').value);
+  luku2 = parseInt(document.getElementById('toinen').value);
   luku3 = parseInt(document.getElementById('kolmas').value);
   document.write("Annoit luvut: " + luku1 + ' ' + luku2 + ' ' + luku3 + "<br>");
   if(luku1 < luku2 && luku1 < luku3)
   {
     if(luku2 < luku3)
     {
-      document.write("Lukujen jÃ¤rjestys: " + luku1 + ' ' + luku2 + ' ' + luku3);
+      document.write("Lukujen järjestys: " + luku1 + ' ' + luku2 + ' ' + luku3);
     }
     else {
-      document.write("Lukujen jÃ¤rjestys: " + luku1 + ' ' + luku3 + ' ' + luku2);
+      document.write("Lukujen järjestys: " + luku1 + ' ' + luku3 + ' ' + luku2);
     }
   }
   else if(luku2 < luku1 && luku2 < luku3)
   {
     if(luku1<luku3)
     {
-      document.write("Lukujen jÃ¤rjestys: " + luku2 + ' ' + luku1 + ' ' + luku3);
+      document.write("Lukujen järjestys: " + luku2 + ' ' + luku1 + ' ' + luku3);
     }
     else {
-      document.write("Lukujen jÃ¤rjestys: " + luku2 + ' ' + luku3 + ' ' + luku1);
+      document.write("Lukujen järjestys: " + luku2 + ' ' + luku3 + ' ' + luku1);
 
     }
   }
@@ -30,10 +30,10 @@ function jarjesta()
     {
       if(luku1 < luku2)
       {
-        document.write("Lukujen jÃ¤rjestys: " + luku3 + ' ' + luku1 + ' ' + luku2);
+        document.write("Lukujen järjestys: " + luku3 + ' ' + luku1 + ' ' + luku2);
       }
       else {
-        document.write("Lukujen jÃ¤rjestys: " + luku3 + ' ' + luku2 + ' ' + luku1);
+        document.write("Lukujen järjestys: " + luku3 + ' ' + luku2 + ' ' + luku1);
 
       }
     }
